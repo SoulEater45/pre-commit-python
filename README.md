46263e9d5eb02caccc9928fa7d92585d5ff7267d
Mirza <mirza.arnaut45@gmail.com>
2022-04-28 22:41:39 +0200

    Add pytest and expand the readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # pre-commit-python
+
+python hooks for [pre-commit](https://pre-commit.com/)
+
+Available hooks:
+  - [unittest](#unittest)
+  - [pytest](#pytest)
+
+## unittest
+Execute `python -m unittest` to check all tests before commiting to the master/main branch.
+
+## pytest
+Execute `python -m pytest` to check all tests before commiting to the master/main branch.
--- scripts/python-pytest.sh
+++ scripts/python-pytest.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-
-# Credit goes to Giacomo Debidda (2022-04-28)
-# Source: https://www.giacomodebidda.com/posts/a-simple-git-hook-for-your-python-projects/
-
 current_branch=`git branch | grep '*' | sed 's/* //'`
 
 if [ "$current_branch" = "master" ] || [ "$current_branch" = "main" ]; then
     echo "You are about to commit on master. I will run your unittest tests first..."
-    python -m unittest
+    python -m pytest
     if [ $? -eq 0 ]; then
         # tests passed, proceed to prepare commit message
         exit 0
